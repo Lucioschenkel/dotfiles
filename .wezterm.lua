0aab010ed08433814fc037828ec983139cc7b359
Lucioschenkel <lucioless@gmail.com>
2024-11-19 05:55:40 -0300

    add lazygit

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Gruvbox Dark (Gogh)'
+config.color_scheme = 'Gruvbox dark, pale (base16)'
 
 config.font_size = 18
 
