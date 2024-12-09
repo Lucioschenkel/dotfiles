e5da1f44e8dd9a39b0602b43110db40bc8f5f77f
Lucioschenkel <lucioless@gmail.com>
2024-12-09 10:36:20 -0300

    feat: hide wezterm tab bar

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration
 local config = wezterm.config_builder()
@@ -7,9 +7,11 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Gruvbox dark, pale (base16)'
+config.color_scheme = "Gruvbox dark, pale (base16)"
 
 config.font_size = 18
+
+config.hide_tab_bar_if_only_one_tab = true
 
 -- and finally, return the configuration to wezterm
 return config
