d06a6192ed9b6b6f0d7c50d763ff0ab398f1c93d
Lucioschenkel <lucio.schenkel@proton.me>
2025-08-27 11:29:17 -0300

    feat(wezterm): remove window decorations and update terminal theme

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,11 +7,18 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "Gruvbox dark, pale (base16)"
+config.color_scheme = "Tokyo Night"
 
 config.font_size = 18
 
 config.hide_tab_bar_if_only_one_tab = true
+
+config.window_frame = {
+	inactive_titlebar_bg = "none",
+	active_titlebar_bg = "none",
+}
+
+config.window_decorations = "RESIZE|MACOS_FORCE_DISABLE_SHADOW"
 
 -- and finally, return the configuration to wezterm
 return config
