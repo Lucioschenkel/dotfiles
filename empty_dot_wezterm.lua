b1de28d3373363624e2625326ad8da96b57fea2b
Lucioschenkel <lucio.schenkel@proton.me>
2026-01-11 09:30:53 -0300

    feat: initial commit

---
+++ empty_dot_wezterm.lua
@@ -0,0 +1,16 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices.
+
+-- or, changing the font size and color scheme.
+config.font_size = 18
+config.color_scheme = 'Tokyo Night'
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+
+-- Finally, return the configuration to wezterm:
+return config
