7a28867ff8a1eb85ec8b5ce37bbc9b313c699c66
Lucioschenkel <lucio.schenkel@proton.me>
2026-01-12 14:20:05 -0300

    feat: update terminal color scheme

--- empty_dot_wezterm.lua
+++ empty_dot_wezterm.lua
@@ -8,7 +8,7 @@ local config = wezterm.config_builder()
 
 -- or, changing the font size and color scheme.
 config.font_size = 18
-config.color_scheme = 'Tokyo Night'
+config.color_scheme = 'Sex Colors (terminal.sexy)'
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 
