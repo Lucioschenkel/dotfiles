2fdeb21f9028a7d270cd987ea467a2c158c88996
Lucioschenkel <lucio.schenkel@proton.me>
2026-01-22 05:22:33 -0300

    chore: update aerospace config

--- empty_dot_wezterm.lua
+++ empty_dot_wezterm.lua
@@ -1,16 +1,10 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices.
-
--- or, changing the font size and color scheme.
 config.font_size = 18
-config.color_scheme = 'Tokyo Night'
+config.color_scheme = 'Sex Colors (terminal.sexy)'
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 
--- Finally, return the configuration to wezterm:
 return config
